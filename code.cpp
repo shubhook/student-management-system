3e5dd87f00159e7e95e89fd78473047278436119
shubhook <khakhashubham@gmail.com>
2025-01-16 22:51:38 +0530

    ADDED A STRUCT

---
+++ code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct info{
+        string name;
+        int roll_no;
+        float cpi;
+        string branch;  
+};
+
+int main(){
+
+    struct info* newStudent = ( struct info* )malloc(sizeof(struct info));
+
+    
+    
+    return 0;
+}
